a8c3fc591e469ccc1438257838a4840be3894b97
KamillySantana <124930683+KamillySantana@users.noreply.github.com>
2023-04-08 16:55:51 -0300

    Delete python directory

--- python/ex7.py
+++
@@ -1,8 +0,0 @@
-base = int(input("Informe a medida da base de um retangulo: "))
-altura = int(input("Informe a medida da altura de um retangulo: "))
-
-perimetro = (base * 2) + (altura * 2)
-area = (base * altura)
-
-print("O valor do perimetro é", perimetro)
-print("O valor da area é", area)
