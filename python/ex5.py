bc9d20125f6b2c46a147e42f89afc12666861dec
KamillySantana <124930683+KamillySantana@users.noreply.github.com>
2023-04-22 16:51:26 -0300

    Delete python directory

--- python/ex5.py
+++
@@ -1,7 +0,0 @@
-numero = int(input("Informe um número de 3 digitos: "))
-
-centena = numero // 100
-dezena = (numero % 100) // 10
-unidade = (numero % 10) 
-
-print(numero, "\nunidade", unidade, "\ndezena", dezena, "\ncentena", centena)
--- python/ex8.py
+++
@@ -1,8 +0,0 @@
-raio = float(input("Informe um valor para o raio de um circulo: "))
-pi = 3.14
-
-circunferencia = (2 * pi * raio)
-area = (raio * raio * pi)
-
-print("Valor da circunferencia é", circunferencia)
-print("Valor da area é", area)
